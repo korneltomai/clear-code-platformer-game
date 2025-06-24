fb1a4de5e0b62d44e92a45150e3e48ba149c94d9
T3tr1s <tomai.kornelmunka@gmail.com>
2025-06-24 16:42:44 +0200

    Add: Platformer logic

--- code/player.py
+++ code/player.py
@@ -9,25 +9,38 @@ class Player(pygame.sprite.Sprite):
 
         # movement
         self.direction = pygame.math.Vector2()
-        self.speed = 400
         self.collision_sprites = collision_sprites
-
+        self.speed = 400
+        self.gravity = 50
+        self.jump_height = 20
+        self.on_ground = False
+        
     def update(self, delta_time):
+        self.check_ground()
         self.handle_input()
         self.move(delta_time)
 
     def handle_input(self):
         keys = pygame.key.get_pressed()
         self.direction.x = int(keys[pygame.K_RIGHT] or keys[pygame.K_d]) - int(keys[pygame.K_LEFT] or keys[pygame.K_a])
-        self.direction.y = int(keys[pygame.K_DOWN] or keys[pygame.K_s]) - int(keys[pygame.K_UP] or keys[pygame.K_w])
-        self.direction = self.direction.normalize() if self.direction else self.direction
+
+        if (keys[pygame.K_UP] or keys[pygame.K_SPACE]) and self.on_ground:
+            self.direction.y = - self.jump_height
 
     def move(self, delta_time):
+        # horizontal
         self.hitbox_rect.x += self.direction.x * self.speed * delta_time
         self.check_collision("horiontal")
-        self.hitbox_rect.y += self.direction.y * self.speed * delta_time
+
+        # vertical
+        self.direction.y += self.gravity * delta_time
+        self.hitbox_rect.y +=  self.direction.y
         self.check_collision("vertical")
         self.rect.center = self.hitbox_rect.center
+
+    def check_ground(self):
+        bottom_rect = pygame.FRect((0,0), (self.hitbox_rect.width - 10, 2)).move_to(midtop = self.hitbox_rect.midbottom)
+        self.on_ground = True if bottom_rect.collidelist([sprite.rect for sprite in self.collision_sprites]) >= 0 else False
 
     def check_collision(self, direction):
         for sprite in self.collision_sprites:
@@ -42,3 +55,4 @@ class Player(pygame.sprite.Sprite):
                         self.hitbox_rect.bottom = sprite.rect.top
                     if self.direction.y < 0:
                         self.hitbox_rect.top = sprite.rect.bottom
+                    self.direction.y = 0
